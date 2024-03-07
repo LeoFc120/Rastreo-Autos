be6981c4ef0ae12bf8bb36d416ca3ebdb28236d5
Leopoldo <leofuenntes@gmail.com>
2024-03-06 18:52:36 -0600

    Agregar archivos de proyecto.

---
+++ Rastreo-Autos/Moto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rastreo_Autos
+{
+    internal class Moto:MedTrasnp
+    {
+        private int cantCilindraje250 { get; set; }
+
+        public Moto(string nom, int pasaje, int cant, int cc250) : base(nom, pasaje, cant)
+
+        {
+            cantCilindraje250 = cc250;
+        }
+
+        public void mostrarmoto()
+        {
+            MostrarMedTrans();
+            Console.WriteLine("Cantidad de vehiculos: " + cantCilindraje250);
+
+        }
+
+    }
+}
